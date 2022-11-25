e2452b93058939c6995b0f5aca172951ca0c3e62
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-11-25 22:55:07 +0600

    added documentation of entity comments via swagger

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -2,26 +2,36 @@ import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CommentEntity } from './entities/comment.entity';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Comments')
 @Controller('comments')
 export class CommentsController {
   constructor(private commentsService: CommentsService) {}
 
+  @ApiOperation({ summary: 'Comment creation' })
+  @ApiResponse({ status: 200, type: CommentEntity })
   @Post()
   create(@Body() commentDto: CreateCommentDto): Promise<CommentEntity> {
     return this.commentsService.createComment(commentDto);
   }
 
+  @ApiOperation({ summary: 'Getting all comments' })
+  @ApiResponse({ status: 200, type: [CommentEntity] })
   @Get()
   getAll(): Promise<CommentEntity[]> {
     return this.commentsService.getAllComments();
   }
 
+  @ApiOperation({ summary: 'Getting one comment' })
+  @ApiResponse({ status: 200, type: CommentEntity })
   @Get(':id')
   getOne(@Param('id') id: string): Promise<CommentEntity> {
     return this.commentsService.getOneComment(+id);
   }
 
+  @ApiOperation({ summary: 'Deleting a comment' })
+  @ApiResponse({ status: 200, type: CommentEntity })
   @Delete(':id')
   remove(@Param('id') id: string): Promise<number> {
     return this.commentsService.removeComment(+id);
--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,13 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('comments')
 export class CommentEntity {
+  @ApiProperty({ example: '1', description: 'Unique identificator' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'Alex Miller', description: 'Author' })
   @Column({ nullable: true, default: 'Anonymous' })
   author?: string;
 
+  @ApiProperty({ example: 'This is great news', description: 'Comment' })
   @Column()
   comment: string;
 }
